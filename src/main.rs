f9a58f5e32a891afeb822233c9eeb5b8fe8fe85c
Linus <khabibtime@rambler.ru>
2024-10-03 12:48:58 +0300

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
 
 fn main() {
-    println!("{}", 0.3 + 0.1); 
+    let x = read_int();
+    println!("{}", x);
+    let name = read_str();
+    println!("{}", name);
+}
+
+fn read_int() -> i128 {
+    let mut num = String::new();   
+    std::io::stdin().read_line(&mut num).expect("error");
+    return num.trim().parse::<i128>().expect("error");
+}
+
+fn read_str() -> String {
+    let mut result = String::new();   
+    std::io::stdin().read_line(&mut result).expect("error");
+    return result;
 }
